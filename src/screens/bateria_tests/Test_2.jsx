d837dcd308b25794a9457cd4ca3a111ee8e08494
Antonio <antongd111@gmail.com>
2024-07-10 17:12:44 +0200

    Test 2 terminado.

--- src/screens/bateria_tests/Test_2.jsx
+++ src/screens/bateria_tests/Test_2.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
-import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import InstruccionesModal from '../../components/instrucciones';
 import clownImage from '../../../assets/images/payaso.png';
 import { Dimensions } from 'react-native';
 import stylesComunes from '../../styles/ComunStyles';
+import { set } from 'date-fns';
 
 const Test_2 = ({ navigation }) => {
     const [modalVisible, setModalVisible] = useState(true);
     const [toques, setToques] = useState(0);
     const [posicion, setPosicion] = useState(obtenerPosicionAleatoria());
+    const [mostrarOpciones, setMostrarOpciones] = useState(false);
+    const [mostrarError, setMostrarError] = useState(false);
 
     function obtenerPosicionAleatoria() {
         const screenWidth = Dimensions.get('window').width;
         const screenHeight = Dimensions.get('window').height;
-    
+
         // Ajusta los márgenes para asegurar que el payaso no se salga de la pantalla
         const margen = 500; // Suponiendo que el payaso tiene un tamaño de 100x100
         const x = Math.floor(Math.random() * (screenWidth - margen));
@@ -25,11 +28,23 @@ const Test_2 = ({ navigation }) => {
     const manejarToquePayaso = () => {
         setToques(toques + 1);
         if (toques >= 2) {
-            navigation.goBack(); // Termina el test
+            setMostrarOpciones(true);
         } else {
             setPosicion(obtenerPosicionAleatoria()); // Actualiza la posición para el siguiente toque
         }
     };
+
+    const seleccionarOpcion = (opcion) => {
+        if (opcion === 'payaso') {
+            // Respuesta correcta
+            navigation.navigate('Test_3');
+        } else {
+            // Respuesta incorrecta
+            setMostrarError(true);
+        }
+    }
+
+
 
     return (
         <View style={stylesComunes.borde_tests}>
@@ -38,15 +53,34 @@ const Test_2 = ({ navigation }) => {
                     visible={modalVisible}
                     onClose={() => setModalVisible(false)}
                     title="Test 2"
-                    instructions="Toca el payaso tan pronto como lo veas."
+                    instructions="Toca el payaso."
                 />
-                {!modalVisible && (
+                {!modalVisible && !mostrarOpciones && (
                     <TouchableOpacity
                         style={[styles.payaso, posicion]}
                         onPress={manejarToquePayaso}
                     >
                         <Image source={clownImage} style={styles.imagenPayaso} />
                     </TouchableOpacity>
+                )}
+                {mostrarOpciones && (
+                    <View style={styles.opcionesContenedor}>
+                        <Text style={styles.instrucciones}>El payaso se llama Bernabé.{'\n'}Toque su tarjeta de visita.</Text>
+                        <View style={styles.opciones} >
+                            <TouchableOpacity style={styles.boton} onPress={() => seleccionarOpcion('payaso')}>
+                                <Text style={styles.nombre}>Bernabé</Text>
+                                <Text style={styles.profesion}>Payaso</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.boton} onPress={() => seleccionarOpcion('emperador')}>
+                            <Text style={styles.nombre}>Julio César</Text>
+                            <Text style={styles.profesion}>Emperador</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                )}
+
+                {mostrarError && (
+                    <Text style={styles.error}>Toque la tarjeta de visita correcta.</Text>
                 )}
             </View>
         </View>
@@ -63,7 +97,53 @@ const styles = StyleSheet.create({
         maxWidth: 350,
         maxHeight: 400,
         resizeMode: 'contain'
-    }
+    },
+    opcionesContenedor: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+    },
+    instrucciones: {
+        fontSize: 40,
+        marginBottom: 20,
+        marginTop: 150,
+        textAlign: 'center',
+    },
+    opciones: {
+        flexDirection: 'row',
+        margin: 10,
+    },
+    boton: {
+        backgroundColor: '#F2E8E1',
+        margin: 50,
+        marginTop: 50,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#D2B48C',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 200,
+        width: 400,
+    },
+    nombre: {
+        color: 'black',
+        fontSize: 40,
+        textAlign: 'center',
+        marginBottom: 40,
+        fontWeight: 'bold',
+    },
+    profesion: {
+        color: 'black',
+        fontSize: 40,
+        textAlign: 'center',
+    },
+    error: {
+        color: 'red',
+        fontSize: 40,
+        textAlign: 'center',
+        marginTop: 80,
+    },
+
 });
 
 export default Test_2;
