43c3ad01581c9f8b1ce489834ca2e3943e60720a
Antonio <antongd111@gmail.com>
2024-09-06 03:49:56 +0200

    Cambio de icono adaptativo

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import Main from './src/screens/Main';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
+import React from 'react';
 import { useEffect } from 'react';
-import { AppLoading } from 'expo';
-import { Text } from 'react-native';
 import initDB from './src/database/db';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-reanimated';
 import * as ScreenOrientation from 'expo-screen-orientation';
 import * as NavigationBar from 'expo-navigation-bar';
-import { StatusBar } from 'react-native';
+import { Platform, StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -20,8 +17,11 @@ export default function App() {
   useEffect(() => {
     // Inicialización la base de datos
     initDB();
-    NavigationBar.setVisibilityAsync("hidden");
-    StatusBar.setHidden(true);
+
+    if (Platform.OS === 'android') {
+      NavigationBar.setVisibilityAsync("hidden");
+      StatusBar.setHidden(true);
+    }
 
     // Bloqueo de la orientación al cargar la app
     lockOrientation();
